5681e1d475628d38bc7e812ba2c89dbc0a88acd5
lucas8 <luc.linux@mailoo.org>
2018-02-21 11:53:51 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+Compiler for a simple functional language
+
---
+++ shell.nix
@@ -0,0 +1,31 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc822" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, array, base, containers, mtl, stdenv
+      , transformers
+      }:
+      mkDerivation {
+        pname = "fc";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          array base containers mtl transformers
+        ];
+        buildDepends = with pkgs; [ cabal-install ];
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
